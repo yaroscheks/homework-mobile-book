3b4b8ce4edfb9f3fde0e23b738bbce7bf7fbd1d9
yaroscheks <yaroscheks@gmail.com>
2024-10-06 10:39:00 +0500

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    private final Map<String, String> contacts = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, number);
+        }
+        return contacts.size();
     }
 }
