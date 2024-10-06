2444f557b9c23c488db3b652caf0719213ca1d7f
yaroscheks <yaroscheks@gmail.com>
2024-10-06 10:55:18 +0500

    Method main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+
+        System.out.println("Добавлено контактов: " + phoneBook.add("A", "1"));
+        System.out.println("Добавлено контактов: " + phoneBook.add("B", "2"));
+        System.out.println("Добавлено контактов: " + phoneBook.add("A", "3"));
+
+        System.out.println("Номер A: " + phoneBook.findByName("A"));
+        System.out.println("Номер B: " + phoneBook.findByName("B"));
+        System.out.println("Номер C: " + phoneBook.findByName("C"));
+
+        System.out.println("Имя по номеру 1: " + phoneBook.findByNumber("1"));
+        System.out.println("Имя по номеру 2: " + phoneBook.findByNumber("2"));
+        System.out.println("Имя по номеру 3: " + phoneBook.findByNumber("3"));
+
+        System.out.println("Все имена:");
+        phoneBook.printAllNames();
+    }
 }
